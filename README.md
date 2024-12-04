ca4eabad6f0564f48f300f78621de532c23f4e3b
Eric Joseph <contato.ericjoseph@gmail.com>
2024-12-04 00:30:06 -0300

    feat: day 2 solved!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+## Advent Of Code - 2024
+
 - [X] Day 1: --- Day 1: Historian Hysteria ---
+- [X] Day 2: --- Day 2: Red-Nosed Reports ---
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,58 @@
 package dev.eric.java
 
 import java.io.File
+import kotlin.math.abs
 
+val splitRgx = Regex("\\s+")
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+
+    val lines = File("src/main/resources/day2.txt")
+        .readLines()
+
+    val day2 = day2(lines)
+
+    println(day2)
+}
+
+fun day2(reports: List<String>): Int {
+    return reports.filter {line ->
+        val report = line.split(splitRgx).map(String::toInt)
+        val diffBetween = report.zipWithNext { a, b ->
+            abs(a - b)
+        }
+
+        if (!diffBetween.all { it in 1..3 }) return@filter false
+
+        val isIncreaseOnly = report.zipWithNext { a, b ->
+            b > a
+        }.all { it }
+
+        val isDecreaseOnly = report.zipWithNext { a, b ->
+            a > b
+        }.all { it }
+
+        isDecreaseOnly || isIncreaseOnly
+    }.size
+}
+
+fun day1(lines: List<String>): Int { // 2970687
     val right = mutableListOf<Int>()
     val left = mutableListOf<Int>()
-    val distancesBetween = mutableListOf<Int>()
+    var distancesBetween = 0
+    lines.forEach { line ->
+        val (r, l) = line.split(splitRgx)
 
-    File("src/main/resources/day1.txt")
-        .readLines()
-        .forEach { line ->
-            val (r, l) = line.split(Regex("\\s+"))
-
-            right.add(r.toInt())
-            left.add(l.toInt())
-        }
-
+        right.add(r.toInt())
+        left.add(l.toInt())
+    }
     right.sort()
     left.sort()
-
     repeat(right.size) {
-        var distance = left[it] - right[it]
-        if (distance < 0) {
-            distance *= -1
-        }
-        distancesBetween.add(distance)
+        val distance = abs(left[it] - right[it])
+        distancesBetween += distance
     }
 
-    println(distancesBetween.sum())
-
+    return distancesBetween
 }
